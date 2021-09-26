03b18c9a3850d80375ac1f53a7df2d705e793685
mas-alissa <89016425+mas-alissa@users.noreply.github.com>
2021-09-26 13:36:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+const makingPizza = function() {
+    console.log("make dough");
+    console.log("knead the dough");
+    console.log("cover the dough in plastic");
+    console.log("check to see if the dough is done");
+    console.log("divide the dough");
+    console.log("shape the dough");
+    console.log("put it in the over for 10 minutes for 200 degree celsus and that's it ");
+    
+}
+
+makingPizza();
+makingPizza();
+
+
+const makingSushi = function(){
+    console.log("cook rice");
+    console.log("Heat your vinegar, sugar, and salt together in a small saucepan")
+    console.log("Add your seasoning");
+    console.log("Cut your filling ingredients into small pieces")
+    console.log("Place a sheet of nori");
+    console.log("Cut your roll into 2.5 cm");
+    console.log("Serve your roll with other toppings")
+
+
+}
+
+makingSushi();
+makingSushi();
+makingSushi();
